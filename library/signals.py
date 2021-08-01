87b3cb155a0a7d4910549ddec279b0a5fe7fd1e6
KennyChrisUmurundi <ndayikennysmuusic@gmail.com>
2021-08-01 21:06:56 +0200

    sdads

--- library/signals.py
+++ library/signals.py
@@ -2,12 +2,13 @@ from django.shortcuts import get_object_or_404
 from .models import Library
 from paypal.standard.ipn.signals import valid_ipn_received
 from django.dispatch import receiver
-from paypal.standard.models import PayPalStandardBase
+# from paypal.standard.models import PayPalStandardBase
+from paypal.standard.ipn.signals import payment_was_successful
 
 
-@receiver(valid_ipn_received)
-def payment_notification(sender, **kwargs):
-    print("sooooooooooooooooooooooooooooooooomeeeeeeeeeeeeeeee")
+def process_subscription(sender, **kwargs):
     ipn_obj = sender
-    lib = PayPalStandardBase.objects.get(id=ipn_obj)
-    print("the liiib",lib)
+    # Undertake some action depending upon `ipn_obj`.
+    print(ipn_obj)
+
+payment_was_successful.connect(process_subscription)
