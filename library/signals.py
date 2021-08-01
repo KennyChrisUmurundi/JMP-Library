f69904b59b897a3856adc9e7ed1914efbe54882f
KennyChrisUmurundi <ndayikennysmuusic@gmail.com>
2021-08-01 21:17:36 +0200

    sak

--- library/signals.py
+++ library/signals.py
@@ -1,5 +1,6 @@
 from paypal.standard.models import ST_PP_COMPLETED
 from paypal.standard.ipn.signals import valid_ipn_received
+from django.dispatch import receiver
 
 @receiver(valid_ipn_received)
 def paypal_payment_received(sender, **kwargs):
